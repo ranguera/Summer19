4caa4793795bc2e88992bcd5af30341e8653d32e
Roger Anguera <ranguera@gmail.com>
2019-07-09 14:34:39 -0700

    init

---
+++ Assets/Scripts/Bump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bump : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            GetComponent<Rigidbody>().AddForce(Vector3.up*1000f);
+
+        if(Input.GetKey(KeyCode.W))
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000f);
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().AddForce(Vector3.back * 1000f);
+        if (Input.GetKey(KeyCode.A))
+            GetComponent<Rigidbody>().AddForce(Vector3.left * 1000f);
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().AddForce(Vector3.right * 1000f);
+    }
+}
